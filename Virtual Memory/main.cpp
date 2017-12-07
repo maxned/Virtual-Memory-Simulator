54a2ed5e98f4ceaab148c204a9019fd7c8ddcd1a
maxned <maxned@yahoo.com>
2017-12-07 14:20:22 -0800

    Initial Commit

---
+++ Virtual Memory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Virtual Memory
+//
+//  Created by Max Nedorezov on 12/7/17.
+//  Copyright © 2017 Max Nedorezov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
